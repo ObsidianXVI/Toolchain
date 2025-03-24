62d9e4a9470483df7f2cbc5133c7f39c220ef925
ObsidianXVI <siddharth.chitikela@gmail.com>
2025-03-24 20:26:56 +0800

    add tool to generate statically-typed API clients from server-side spec can't go wrong with static typing on both ends, am I right?

---
+++ dart_tools/boilerplates/http_apis/generate/lib/api_client.dart
@@ -0,0 +1,123 @@
+library toolchain.dart.boilerplate.http_apis.generate.script;
+
+import 'dart:io';
+
+import 'package:http_apis_define/http_apis.dart';
+
+/// 1. Define the API class using the http_apis_define package
+/// 2. Copy+paste the definition into the global [api] variable below
+/// 3. Replace all `handleRequest` declarations with [emptyHandler]
+/// 4. Run the script
+/// 5. Copy+paste the file(s) from the lib/output folder into your client-side package
+Future<int> emptyHandler(
+        {required T? Function<T>(String) getParam,
+        required void Function(int, String) raise}) async =>
+    0;
+late final API api;
+void main(List<String> args) async {
+  final String intermediateClientName = api.apiName.split('_').join();
+  final String clientName =
+      '${intermediateClientName[0].toUpperCase()}${intermediateClientName.substring(1)}';
+  final StringBuffer generated = StringBuffer("""import 'dart:convert';
+import 'package:http/http.dart' as http;
+
+class $clientName {
+  static const String apiName = '${api.apiName}';
+  static const String apiHost = 'apis.obsivision.com';
+
+""");
+  final StringBuffer classDefinitions = StringBuffer();
+  final (List<Endpoint>, List<String>) endpointsAndRoutes =
+      traverseRoutes(api.routes, '/${api.apiName.split('_')[0]}');
+  if (endpointsAndRoutes.$1.isNotEmpty) {
+    for (int i = 0; i < endpointsAndRoutes.$1.length; i++) {
+      final Endpoint endpoint = endpointsAndRoutes.$1[i];
+      final String routePath = endpointsAndRoutes.$2[i].substring(1);
+      final String intermediateName = routePath
+          .split(RegExp(r'[/-]'))
+          .map((route) => '${route[0].toUpperCase()}${route.substring(1)}')
+          .join();
+      final String endpointName =
+          '${intermediateName[0].toLowerCase()}${intermediateName.substring(1)}';
+      final String className = '${intermediateName}Endpoint';
+      generated.writeln("  static final $endpointName = $className();");
+      classDefinitions.writeln("class $className {");
+      final String qParams = [
+        for (final qparam in endpoint.queryParameters)
+          "${qparam.encodedType} ${qparam.name},"
+      ].join('\n');
+      for (final endpointType in endpoint.endpointTypes) {
+        classDefinitions.write("""
+  Future<http.Response> ${endpointType.method.toLowerCase()}({
+    required ({
+      $qParams
+    }) queryParameters,
+    """);
+        if (![EndpointType.delete, EndpointType.get].contains(endpointType) &&
+            endpoint.bodyParameters != null) {
+          final String bParams = [
+            for (final bparam in endpoint.bodyParameters!)
+              "${bparam.encodedType} ${bparam.name},"
+          ].join('\n');
+          classDefinitions.write("""
+required ({
+      $bParams
+    }) bodyParameters,
+  
+""");
+        }
+        classDefinitions.write("""}) async {
+    return await http.${endpointType.method.toLowerCase()}(
+      Uri.https(
+        'apis.obsivision.com',
+        '$routePath',
+        {
+          ${[
+          for (final qparam in endpoint.queryParameters)
+            "'${qparam.name}': queryParameters.${qparam.name},",
+        ].join('\n')}
+        },
+      ),
+    """);
+        if (![EndpointType.delete, EndpointType.get].contains(endpointType) &&
+            endpoint.bodyParameters != null) {
+          classDefinitions.write("""
+body: jsonEncode({
+  
+          ${[
+            for (final bparam in endpoint.bodyParameters!)
+              "'${bparam.name}': bodyParameters.${bparam.name},",
+          ].join('\n')}
+}),
+""");
+        }
+        classDefinitions.writeln(");\n}\n}");
+      }
+    }
+  }
+  generated
+    ..writeln("}")
+    ..write(classDefinitions.toString());
+
+  await (await File('./lib/output/${api.apiName}_client.g.dart').create())
+      .writeAsString(generated.toString());
+}
+
+(List<Endpoint>, List<String>) traverseRoutes(
+    List<RouteSegment> routes, String parent) {
+  if (routes.isEmpty) return const ([], []);
+  final (List<Endpoint>, List<String>) results = ([], []);
+  for (final route in routes) {
+    if (route.endpoint == null) {
+      final tmp = traverseRoutes(route.routes!, '$parent/${route.routeName}');
+      results
+        ..$1.addAll(tmp.$1)
+        ..$2.addAll(tmp.$2);
+    } else {
+      results
+        ..$1.add(route.endpoint!)
+        ..$2.add('$parent/${route.routeName}');
+    }
+  }
+  return results;
+}
